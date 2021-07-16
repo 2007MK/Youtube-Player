95483ccd4ff989eb758dd44cde937dd12a958484
Saksham Shekher <67940483+Oshekher@users.noreply.github.com>
2021-07-16 11:25:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function play() {
         console.log(url)
         url = url.replace("https://www.youtube.com/watch?v=","https://www.youtube.com/embed/");
         console.log(url)
-        var iframe = document.getElementById('iframe');
+        var iframe = document.getElementById('framez');
         var src = document.createAttribute("src");
         src.value = url;
         iframe.setAttributeNode(src);
